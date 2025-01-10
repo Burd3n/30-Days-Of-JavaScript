838a0aecd7bd47571fbeaf3a5797e5b56ee87a01
Super User <yang_run1996@outlook.com>
2025-01-10 22:31:25 +0800

    finished level 1 exercises for day 2

---
+++ 02_Day_Data_types/ex_lvl1.js
@@ -0,0 +1,88 @@
+// ex 1
+let challenge = "30 Days of JavaScript"
+
+// ex 2
+// console.log(challenge) 
+
+// ex 3
+// console.log(challenge.length) 
+
+// ex 4
+// console.log(challenge.toUpperCase()) 
+
+// ex 5
+// console.log(challenge.toLowerCase()) 
+
+// ex 6
+// console.log(challenge.substring(3, 7)) 
+// console.log(challenge.substr(3, 15))
+
+// ex 7
+// console.log(challenge.slice(3))
+
+// ex 8
+// console.log(challenge)
+// console.log(challenge.includes("Script"))
+
+// ex 9
+// console.log(challenge)
+// console.log(challenge.split())
+// console.log(challenge.split(""))
+
+// ex 10
+// console.log(challenge.split(" "))
+
+// ex 11
+// let tech = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+// console.log(tech.split(", "))
+
+// ex 12
+// console.log(challenge)
+// console.log(challenge.replace("JavaScript", "Python"))
+
+// ex 13
+// console.log(challenge.charAt(15))
+
+// ex 14
+// console.log(challenge.charCodeAt("J"))
+
+// ex 15
+// console.log(challenge.indexOf("a"))
+
+// ex 16
+// console.log(challenge.lastIndexOf("a"))
+
+// ex 17
+// let sentence = "You cannot end a sentence with because because because is a conjunction"
+// console.log(sentence.indexOf("because"))
+
+// ex 18
+// let sentence = "You cannot end a sentence with because because because is a conjunction"
+// console.log(sentence.lastIndexOf("because"))
+
+// ex 19
+// let sentence = "You cannot end a sentence with because because because is a conjunction"
+// console.log(sentence.search("because"))
+
+// ex 20
+// let sentence = ' 30 Days Of JavaScript '
+// console.log(sentence.trim())
+
+// ex 21
+// console.log(challenge)
+// console.log(challenge.startsWith("30"))
+
+// ex 22
+// console.log(challenge.endsWith("JavaScript"))
+
+// ex 23
+// console.log(challenge.match(/a/gi))
+
+// ex 24
+// let sub1 = "30 Days of "
+// let sub2 = "JavaScript"
+
+// console.log(sub1.concat(sub2))
+
+// ex 25
+console.log(challenge.repeat(2))
