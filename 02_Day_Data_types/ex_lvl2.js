6c9510a2cdef2edeba985e09959b2d0f905a984f
Super User <yang_run1996@outlook.com>
2025-03-15 17:57:41 +0800

    added progress

---
+++ 02_Day_Data_types/ex_lvl2.js
@@ -0,0 +1,18 @@
+// q1
+//console.log("The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.")
+
+// q2
+/*
+let var1 = '10'
+let var2 = 10
+
+console.log(typeof(+var1) == typeof(var2))
+*/
+
+// q7
+/*
+let numRand = Math.random()
+let randHundred = numRand * 101
+let roundHundred = Math.floor(randHundred)
+console.log(roundHundred)
+*/
